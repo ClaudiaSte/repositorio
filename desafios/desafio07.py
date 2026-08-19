eecd2e5ce7a7b675cb694deae1de73a61146d1d1
ClaudiaSte <grandevale2020@gmail.com>
2026-08-19 19:46:12 -0300

    atualização do exercício sete

--- desafios/desafio07.py
+++ desafios/desafio07.py
@@ -2,4 +2,4 @@ n1 = int(input('Um valor: '))
 dobro = n1 * 2
 triplo = n1 * 3
 raiz = n1 ** 0.5
-print('O dobro de {} é {}, o triplo é {} e a raiz quadrada é {:.2f}'.format(n1, dobro, triplo, raiz))
+print('O dobro de {} é {}.\nO triplo é {}. \nE a raiz quadrada é {:.2f}'.format(n1, dobro, triplo, raiz))
