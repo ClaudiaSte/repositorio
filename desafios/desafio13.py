377164d563bea2eb834a851320c4a503a588e07e
ClaudiaSte <grandevale2020@gmail.com>
2026-08-24 20:07:43 -0300

    atualização

--- desafios/desafio13.py
+++ desafios/desafio13.py
@@ -1,3 +1,3 @@
 prod = float(input('Digite o preço do produto: R$ '))
-desc = prod * 0.05
-print(f'O produto que custava R$ {prod:.2f}, na promoção com desconto de 5% vai custar R$ {prod - desc:.2f}.')
+desc = prod - ( prod * 5 / 100)
+print(f'O produto que custava R$ {prod:.2f}, na promoção com desconto de 5% vai custar R$ {desc:.2f}.')
