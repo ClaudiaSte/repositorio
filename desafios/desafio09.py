9c940b4abebb95227251393b23b24c5a3d82676f
ClaudiaSte <grandevale2020@gmail.com>
2026-08-24 19:44:03 -0300

    atualização

--- desafios/desafio09.py
+++ desafios/desafio09.py
@@ -1,2 +1,2 @@
 v = float(input('Digite a quantidade de metros: '))
-print(f'A quantidade de metros em centímetros é {v * 100} e em milímetros é {v * 1000}')
+print(f'A quantidade de metros em centímetros é {v * 100}cm e em milímetros é {v * 1000}mm')
