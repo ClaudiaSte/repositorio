2e57c45c89d9f19c4bb226fc913d7707e29109df
ClaudiaSte <grandevale2020@gmail.com>
2026-08-25 19:46:45 -0300

    exercício 14

--- desafios/desafio14.py
+++ desafios/desafio14.py
@@ -1,1 +1,3 @@
-sal = 
+sal = float(input("Digite o salário do funcionário: "))
+aumento = sal + (sal * 15 / 100)
+print(f'O funcionário que ganhava R$ {sal:.2f}, com 15% de aumento, passa a receber R$ {aumento:.2f}.')
