2880b7f352a60f3b85c7d9d8be2badb2e132d12e
ClaudiaSte <grandevale2020@gmail.com>
2026-09-14 19:56:26 -0300

    exercícios corrigidos

--- desafios/desafio23.py
+++ desafios/desafio23.py
@@ -1,6 +1,7 @@
-n = input('Digite seu nome: ')
+n = str(input('Digite seu nome completo: ')).strip()
 
-print('Seu nome com todas as letras maiúsculas é:', n.upper())
-print('Seu nome com todas as letras minúsculas é: ', n.lower())
-print('Seu nome tem ao todo ', len(n), 'letras.')
-print('Seu primeiro nome tem ', n.find(' '), 'letras.')
+print('Analisando seu nome...')
+print('Seu nome com todas as letras maiúsculas é{}:'.format(n.upper()))
+print('Seu nome com todas as letras minúsculas é{}:'.format(n.lower()))
+print('Seu nome tem ao todo {} letras.'.format(len(n) - n.count(' ')))
+print('Seu primeiro nome tem {} letras.'.format(n.find(' ')))
--- desafios/desafio25.py
+++ desafios/desafio25.py
@@ -1,2 +1,2 @@
-cid = input('Digite o nome da cidade: ')
-print('A cidade começa com "Santo"?', 'Santo' in cid.strip().title())
+cid = str(input('Digite o nome da cidade: ')).strip()
+print(cid[:5].upper() == 'SANTO')
